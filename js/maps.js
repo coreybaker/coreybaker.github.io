1b8fcd42266280c8c7d5e417c15f12086863abeb
Corey Baker <coreydavidbaker@gmail.com>
2014-03-02 18:12:41 -0600

    put in esri map and such

---
+++ js/maps.js
@@ -0,0 +1,14 @@
+require(["esri/map", "dojo/domReady!"], function(Map){
+
+
+var map = new Map("mapDiv", {
+	center: [-56.049, 38.485],
+	zoom: 4, 
+	basemap: "osm"
+
+
+}); //end map creation
+
+
+
+}); // end all requires
