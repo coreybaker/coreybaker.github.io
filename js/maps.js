baa457536b95625b83265e83f9ad3ebc3b974335
Corey Baker <coreydavidbaker@gmail.com>
2014-03-08 13:56:55 -0600

    changed to agol map instead

--- js/maps.js
+++ js/maps.js
@@ -1,22 +1,42 @@
+var _map; 
+var _layers; 
+var _targetLayer;
+var WEBMAPID = "1f619a0e47d64d42a592f5821754ba93"; 
+
+require(["esri/map", "esri/arcgis/utils","dojo/domReady!"], 
+
+function(Map, arcgisUtils){
 
 
-require(["esri/map", "esri/geometry/Point","dojo/domReady!"], 
+arcgisUtils.createMap(WEBMAPID, "mapDiv",{
+	mapOptions: {
+		slider:false,
+		wrapAround180: false
+	},
+}).then(function(response){
+			_map = response.map; 
 
-function(Map,Point){
+		_layers = response.itemInfo.itemData.operationalLayers;
+
+		//holy cow this actually returned what I was looking for
+		console.log(_layers);
+
+		_targetLayer = _layers[0];
 
 
-var map = new Map("mapDiv", {
-	center: [-88.985918, 40.509185],
-	zoom: 16, 
-	basemap: "osm"
+		// var what = $("#map-header").append(_targetLayer.popupInfo.title);
+
+		// var otherElemtns = $("<p>test</p>").appendTo("#map-header");
+
+		// var elements = _targetLayer.popupInfo.fieldInfos[1].fieldName;
+
+		// for (var i = 0; i < elements.length; i++) {
+		// 	console.log(elements); 
+		// }
 
 
 
-
-}); //end map creation
-
-var point = new Point( {"x": -88.98, "y": 40.509, "spatialReference": {"wkid": 4326 } });
-
+	}); 
 
 
 }); // end all requires
