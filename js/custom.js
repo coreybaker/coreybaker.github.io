5c483fb79073daf5b982f67e1dd8056e236c9d0e
Corey Baker <coreydavidbaker@gmail.com>
2014-04-12 16:14:18 -0500

    changed the index picture size

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+
+
+
+//if the width of the topbar is less than 1219 pixels 
+//then I want to take the menu and drop it below of 
+// the topbar in it's out section 
+
+/* if $(".top-bar").width < 1219px {
+	$(".top-bar-section").hide();
+	$(".top-bar-sectino-mobile").show();
+}
+
+
+*/
+
