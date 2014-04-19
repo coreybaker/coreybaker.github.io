f9589309ee53df5b598050764e13195898ba2360
Corey Baker <coreydavidbaker@gmail.com>
2014-04-19 13:41:48 -0500

    made some changes to act page

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,7 @@
 
 
 
-//if the width of the topbar is less than 1219 pixels 
-//then I want to take the menu and drop it below of 
-// the topbar in it's out section 
-
-/* if $(".top-bar").width < 1219px {
-	$(".top-bar-section").hide();
-	$(".top-bar-sectino-mobile").show();
-}
-
-
-*/
+// need to do something with the registry 
+// remove class text-center, add class text-left for the 
+// registry information.  When the browser is in mobile mode 
 
